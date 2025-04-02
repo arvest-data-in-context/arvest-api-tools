5f1ee275d7292bf1ab85f0395232d6440d54fc05
Ivan.D <aryodeliae@gmail.com>
2025-04-02 16:34:59 +0200

    Essai 1

--- arvesttools/manifest_creation/__init__.py
+++ arvesttools/manifest_creation/__init__.py
@@ -1,1 +1,2 @@
 from .create_manifest import media_to_manifest
+from .create_manifest import ouaf
--- arvesttools/manifest_creation/create_manifest.py
+++ arvesttools/manifest_creation/create_manifest.py
@@ -1,2 +1,7 @@
 def media_to_manifest(path):
     print(path)
+
+
+def ouaf(krak):
+    print(krak)
+
